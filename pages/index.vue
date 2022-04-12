22b4afd89883857a9767c8fb83c61be71186352b
Pranav Bodawala <b.pranav.k@gmail.com>
2022-04-11 22:01:20 -0600

    create site under construction page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>Hello World</div>
+  <main>
+    <section class="max-w-screen-md mx-auto prose text-center">
+      <div class="w-52 mx-auto">
+        <img src="/logo.svg" width="100%" />
+      </div>
+      <h1 class="">Coming Soon</h1>
+      <p class="">
+        Our website is under construction. <br />
+        Please contact us at
+        <a
+          href="mailto:hustleaservices@gmail.com"
+          class="text-red-500 underline hover:no-underline"
+          >hustleaservices@gmail.com</a
+        >
+        or call
+        <a
+          href="tel:4034020462"
+          class="text-red-500 underline hover:no-underline"
+          >403-402-0462</a
+        >
+      </p>
+    </section>
+  </main>
 </template>
 
 <script>
