4e846c6a22b99a86fb780a1b60bf1d7e2ebcbc7a
Pranav Bodawala <b.pranav.k@gmail.com>
2022-09-04 23:11:51 -0600

    Revert "removed asterisk attribute from lablel elements"
    This reverts commit 98e89393548e92708b2fd0f38bc901d2dab5c1c8.

--- pages/book-now-stage-2.jsx
+++ pages/book-now-stage-2.jsx
@@ -1,22 +1,22 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-import Layout from '../components/layout/Layout';
-import Button from '../components/ui/Button';
-import Card from '../components/ui/Card';
-import CustomErrorMessage from '../components/ui/CustomErrorMessage';
-import PageSection from '../components/ui/PageSection';
-import useLocalStorage from '../hooks/useLocalStorage';
-import { calcTotalPriceOfServices } from '../utils/utils';
+import Layout from "../components/layout/Layout";
+import Button from "../components/ui/Button";
+import Card from "../components/ui/Card";
+import CustomErrorMessage from "../components/ui/CustomErrorMessage";
+import PageSection from "../components/ui/PageSection";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { calcTotalPriceOfServices } from "../utils/utils";
 
 const initialValues = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  phoneNumber: '',
-  carDetails: '',
-  preferredServiceDate: '',
-  message: '',
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  carDetails: "",
+  preferredServiceDate: "",
+  message: "",
 };
 
 const phoneRegExp =
@@ -24,13 +24,13 @@ const phoneRegExp =
 
 const validationSchema = Yup.object({
   firstName: Yup.string()
-    .max(15, 'Must be 15 characters or less')
-    .required('Required'),
-  lastName: Yup.string().max(20, 'Must be 20 characters or less'),
-  email: Yup.string().email('Invalid email address').required('Required'),
-  phoneNumber: Yup.string().matches(phoneRegExp, 'Invalid phone number'),
-  carDetails: Yup.string().required('Required'),
-  preferredServiceDate: Yup.string().required('Required'),
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  lastName: Yup.string().max(20, "Must be 20 characters or less"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  phoneNumber: Yup.string().matches(phoneRegExp, "Invalid phone number"),
+  carDetails: Yup.string().required("Required"),
+  preferredServiceDate: Yup.string().required("Required"),
   message: Yup.string(),
 });
 
@@ -42,7 +42,7 @@ const isRequired = (field) => {
 
 const BookNowStage2 = () => {
   const [userServiceSelections, _] = useLocalStorage(
-    'user-service-selections',
+    "user-service-selections",
     null
   );
 
@@ -59,9 +59,9 @@ const BookNowStage2 = () => {
     addOns: userServiceSelections?.addOns,
   });
 
-  let selectedCarTypeString = 'No Car Type Selected';
-  let selectedServiceString = 'No Service Selected';
-  let selectedAddOnsString = 'No Add Ons Selected';
+  let selectedCarTypeString = "No Car Type Selected";
+  let selectedServiceString = "No Service Selected";
+  let selectedAddOnsString = "No Add Ons Selected";
 
   if (userServiceSelections) {
     const { carType, service, addOns } = userServiceSelections;
@@ -77,166 +77,205 @@ const BookNowStage2 = () => {
     if (addOns && addOns.length > 0) {
       selectedAddOnsString = addOns
         .map((addOn) => `${addOn.label} ($${addOn.price})`)
-        .join(', ');
+        .join(", ");
     }
   }
 
   return (
     <Layout>
       <main>
-        <h2 className='page-heading'>Book Now</h2>
+        <h2 className="page-heading">Book Now</h2>
         <PageSection>
-          <section className='grid grid-cols-3 gap-10'>
+          <section className="grid grid-cols-3 gap-10">
             <article>
               <Card>
-                <div className='card-body'>
-                  <h3 className='font-bold card-title'>Summary</h3>
-                  <div className='card-text'>
-                    <section className='space-y-1'>
+                <div className="card-body">
+                  <h3 className="card-title font-bold">Summary</h3>
+                  <div className="card-text">
+                    <section className="space-y-1">
                       <p>
                         <span>Car Type: </span>
-                        <span className='font-semibold'>
+                        <span className="font-semibold">
                           {selectedCarTypeString}
                         </span>
                       </p>
                       <p>
                         <span>Selected Package: </span>
-                        <span className='font-semibold'>
+                        <span className="font-semibold">
                           {selectedServiceString}
                         </span>
                       </p>
                       <p>
                         <span>Add-Ons: </span>
-                        <span className='font-semibold'>
+                        <span className="font-semibold">
                           {selectedAddOnsString}
                         </span>
                       </p>
                     </section>
-                    <section className='mt-3'>
-                      <hr className='my-3' />
-                      <p className='font-bold'>
-                        Total:{' '}
-                        <span className='text-accent'> ${totalPrice} </span>
+                    <section className="mt-3">
+                      <hr className="my-3" />
+                      <p className="font-bold">
+                        Total:{" "}
+                        <span className="text-accent"> ${totalPrice} </span>
                       </p>
                     </section>
                   </div>
                 </div>
               </Card>
             </article>
-            <article className='col-span-2'>
-              <div className='text-center heading'>Contact Info</div>
+            <article className="col-span-2">
+              <div className="heading text-center">Contact Info</div>
               <Formik
                 initialValues={initialValues}
                 validationSchema={validationSchema}
                 onSubmit={handleOnSubmit}
               >
-                <Form className='space-y-5'>
-                  <section className='grid grid-cols-2 gap-5'>
-                    <div className='form-group'>
-                      <label htmlFor='firstName' className='form-label'>
+                <Form className="space-y-5">
+                  <section className="grid grid-cols-2 gap-5">
+                    <div className="form-group">
+                      <label
+                        htmlFor="firstName"
+                        className="form-label"
+                        asterisk={isRequired({
+                          name: "firstName",
+                        }).toString()}
+                      >
                         First Name
                       </label>
                       <Field
-                        name='firstName'
-                        type='text'
-                        className='form-control'
+                        name="firstName"
+                        type="text"
+                        className="form-control"
                       />
                       <ErrorMessage
-                        name='firstName'
+                        name="firstName"
                         render={CustomErrorMessage}
                       />
                     </div>
-                    <div className='form-group'>
-                      <label htmlFor='lastName' className='form-label'>
+                    <div className="form-group">
+                      <label
+                        htmlFor="lastName"
+                        className="form-label"
+                        asterisk={isRequired({
+                          name: "lastName",
+                        }).toString()}
+                      >
                         Last Name
                       </label>
                       <Field
-                        name='lastName'
-                        type='text'
-                        className='form-control'
+                        name="lastName"
+                        type="text"
+                        className="form-control"
                       />
                       <ErrorMessage
-                        name='lastName'
+                        name="lastName"
                         render={CustomErrorMessage}
                       />
                     </div>
                   </section>
 
-                  <section className='grid grid-cols-2 gap-5'>
-                    <div className='form-group'>
-                      <label htmlFor='email' className='form-label'>
+                  <section className="grid grid-cols-2 gap-5">
+                    <div className="form-group">
+                      <label
+                        htmlFor="email"
+                        className="form-label"
+                        asterisk={isRequired({
+                          name: "email",
+                        }).toString()}
+                      >
                         Email Address
                       </label>
                       <Field
-                        name='email'
-                        type='email'
-                        className='form-control'
+                        name="email"
+                        type="email"
+                        className="form-control"
                       />
-                      <ErrorMessage name='email' render={CustomErrorMessage} />
+                      <ErrorMessage name="email" render={CustomErrorMessage} />
                     </div>
-                    <div className='form-group'>
-                      <label htmlFor='phoneNumber' className='form-label'>
+                    <div className="form-group">
+                      <label
+                        htmlFor="phoneNumber"
+                        className="form-label"
+                        asterisk={isRequired({
+                          name: "phoneNumber",
+                        }).toString()}
+                      >
                         Phone Number
                       </label>
                       <Field
-                        name='phoneNumber'
-                        type='text'
-                        className='form-control'
+                        name="phoneNumber"
+                        type="text"
+                        className="form-control"
                       />
                       <ErrorMessage
-                        name='phoneNumber'
+                        name="phoneNumber"
                         render={CustomErrorMessage}
                       />
                     </div>
                   </section>
 
-                  <section className='grid grid-cols-2 gap-5'>
-                    <div className='form-group'>
-                      <label htmlFor='carDetails' className='form-label'>
+                  <section className="grid grid-cols-2 gap-5">
+                    <div className="form-group">
+                      <label
+                        htmlFor="carDetails"
+                        className="form-label"
+                        asterisk={isRequired({
+                          name: "carDetails",
+                        }).toString()}
+                      >
                         Car Details
                       </label>
                       <Field
-                        name='carDetails'
-                        type='text'
-                        className='form-control'
+                        name="carDetails"
+                        type="text"
+                        className="form-control"
                       />
                       <ErrorMessage
-                        name='carDetails'
+                        name="carDetails"
                         render={CustomErrorMessage}
                       />
                     </div>
 
-                    <div className='form-group'>
+                    <div className="form-group">
                       <label
-                        htmlFor='preferredServiceDate'
-                        className='form-label'
+                        htmlFor="preferredServiceDate"
+                        className="form-label"
+                        asterisk={isRequired({
+                          name: "preferredServiceDate",
+                        }).toString()}
                       >
                         Preferred Service Date
                       </label>
                       <Field
-                        name='preferredServiceDate'
-                        type='date'
-                        className='form-control'
+                        name="preferredServiceDate"
+                        type="date"
+                        className="form-control"
                       />
                       <ErrorMessage
-                        name='preferredServiceDate'
+                        name="preferredServiceDate"
                         render={CustomErrorMessage}
                       />
                     </div>
                   </section>
-                  <div className='form-group'>
-                    <label htmlFor='message' className='form-label'>
+                  <div className="form-group">
+                    <label
+                      htmlFor="message"
+                      className="form-label"
+                      asterisk={isRequired({
+                        name: "message",
+                      }).toString()}
+                    >
                       Message
                     </label>
                     <Field
-                      name='message'
-                      as='textarea'
-                      className='form-control'
+                      name="message"
+                      as="textarea"
+                      className="form-control"
                     />
-                    <ErrorMessage name='message' render={CustomErrorMessage} />
+                    <ErrorMessage name="message" render={CustomErrorMessage} />
                   </div>
 
-                  <div className='flex justify-end mt-3'>
+                  <div className="mt-3 flex justify-end">
                     <Button submit accent>
                       Request Service
                     </Button>
