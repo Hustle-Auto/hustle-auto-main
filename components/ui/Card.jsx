4848435b386088d7c32b495f2757e023fb398bdc
Pranav Bodawala <b.pranav.k@gmail.com>
2022-09-05 00:34:49 -0600

    add back ability to add more classes to Card component because RadioCardInput and CheckboxCard Input use it

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,6 +1,13 @@
-const Card = ({ children }) => {
+import classnames from "classnames";
+
+const Card = ({ children, className }) => {
   return (
-    <div className="border border-gray-100 rounded-lg shadow-sm">
+    <div
+      className={classnames(
+        "border border-gray-100 rounded-lg shadow-sm",
+        className
+      )}
+    >
       {children}
     </div>
   );
