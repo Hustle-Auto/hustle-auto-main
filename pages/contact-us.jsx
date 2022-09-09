b11117b6fea5ff7355d6ebbf74ea87e969eb2241
Pranav Bodawala <b.pranav.k@gmail.com>
2022-09-08 21:42:24 -0600

    add contact info card to contact us page, make email address required, phone number optional, message required

--- pages/contact-us.jsx
+++ pages/contact-us.jsx
@@ -34,11 +34,9 @@ const validationSchema = Yup.object({
     .max(15, "Must be 15 characters or less")
     .required("Required"),
   lastName: Yup.string().max(20, "Must be 20 characters or less"),
-  email: Yup.string().email("Invalid email address"),
-  phoneNumber: Yup.string()
-    .matches(phoneRegExp, "Invalid phone number")
-    .required("Required"),
-  message: Yup.string(),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  phoneNumber: Yup.string().matches(phoneRegExp, "Invalid phone number"),
+  message: Yup.string().required("Required"),
 });
 
 const isRequired = (field) => {
@@ -87,107 +85,148 @@ const ContactUs = () => {
         <PageSection>
           <h2 className="page-heading">Contact Us</h2>
 
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleOnSubmit}
-          >
-            <Form className="space-y-5">
-              <section className="grid grid-cols-2 gap-5">
-                <div className="form-group">
-                  <label
-                    htmlFor="firstName"
-                    className="form-label"
-                    // eslint-disable-next-line react/no-unknown-property
-                    asterisk={isRequired({
-                      name: "firstName",
-                    }).toString()}
-                  >
-                    First Name
-                  </label>
-                  <Field
-                    name="firstName"
-                    type="text"
-                    className="form-control"
-                  />
-                  <ErrorMessage name="firstName" render={CustomErrorMessage} />
+          <section className="grid grid-cols-3 gap-10 my-10">
+            <article>
+              <Card>
+                <div className="card-body">
+                  <h3 className="font-bold card-title">Contact Info</h3>
+                  <div className="card-text">
+                    <section className="space-y-1">
+                      <p>hustleaservices@gmail.com</p>
+                      <p>(587) 436 - 0142</p>
+                    </section>
+                  </div>
                 </div>
-                <div className="form-group">
-                  <label
-                    htmlFor="lastName"
-                    className="form-label"
-                    // eslint-disable-next-line react/no-unknown-property
-                    asterisk={isRequired({
-                      name: "lastName",
-                    }).toString()}
-                  >
-                    Last Name
-                  </label>
-                  <Field name="lastName" type="text" className="form-control" />
-                  <ErrorMessage name="lastName" render={CustomErrorMessage} />
-                </div>
-              </section>
+              </Card>
+            </article>
+            <article className="col-span-2">
+              <div className="text-center heading">Send Us A Message</div>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleOnSubmit}
+              >
+                <Form className="space-y-5">
+                  <section className="grid grid-cols-2 gap-5">
+                    <div className="form-group">
+                      <label
+                        htmlFor="firstName"
+                        className="form-label"
+                        // eslint-disable-next-line react/no-unknown-property
+                        asterisk={isRequired({
+                          name: "firstName",
+                        }).toString()}
+                      >
+                        First Name
+                      </label>
+                      <Field
+                        name="firstName"
+                        type="text"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="firstName"
+                        render={CustomErrorMessage}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label
+                        htmlFor="lastName"
+                        className="form-label"
+                        // eslint-disable-next-line react/no-unknown-property
+                        asterisk={isRequired({
+                          name: "lastName",
+                        }).toString()}
+                      >
+                        Last Name
+                      </label>
+                      <Field
+                        name="lastName"
+                        type="text"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="lastName"
+                        render={CustomErrorMessage}
+                      />
+                    </div>
+                  </section>
 
-              <section className="grid grid-cols-2 gap-5">
-                <div className="form-group">
-                  <label
-                    htmlFor="phoneNumber"
-                    className="form-label"
-                    // eslint-disable-next-line react/no-unknown-property
-                    asterisk={isRequired({
-                      name: "phoneNumber",
-                    }).toString()}
-                  >
-                    Phone Number
-                  </label>
-                  <Field
-                    name="phoneNumber"
-                    type="text"
-                    className="form-control"
-                  />
-                  <ErrorMessage
-                    name="phoneNumber"
-                    render={CustomErrorMessage}
-                  />
-                </div>
-                <div className="form-group">
-                  <label
-                    htmlFor="email"
-                    className="form-label"
-                    // eslint-disable-next-line react/no-unknown-property
-                    asterisk={isRequired({
-                      name: "email",
-                    }).toString()}
-                  >
-                    Email Address
-                  </label>
-                  <Field name="email" type="email" className="form-control" />
-                  <ErrorMessage name="email" render={CustomErrorMessage} />
-                </div>
-              </section>
+                  <section className="grid grid-cols-2 gap-5">
+                    <div className="form-group">
+                      <label
+                        htmlFor="email"
+                        className="form-label"
+                        // eslint-disable-next-line react/no-unknown-property
+                        asterisk={isRequired({
+                          name: "email",
+                        }).toString()}
+                      >
+                        Email Address
+                      </label>
+                      <Field
+                        name="email"
+                        type="email"
+                        className="form-control"
+                      />
+                      <ErrorMessage name="email" render={CustomErrorMessage} />
+                    </div>
+                    <div className="form-group">
+                      <label
+                        htmlFor="phoneNumber"
+                        className="form-label"
+                        // eslint-disable-next-line react/no-unknown-property
+                        asterisk={isRequired({
+                          name: "phoneNumber",
+                        }).toString()}
+                      >
+                        Phone Number
+                      </label>
+                      <Field
+                        name="phoneNumber"
+                        type="text"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="phoneNumber"
+                        render={CustomErrorMessage}
+                      />
+                    </div>
+                  </section>
 
-              <div className="form-group">
-                <label
-                  htmlFor="message"
-                  className="form-label"
-                  // eslint-disable-next-line react/no-unknown-property
-                  asterisk={isRequired({
-                    name: "message",
-                  }).toString()}
-                >
-                  Message
-                </label>
-                <Field name="message" as="textarea" className="form-control" />
-                <ErrorMessage name="message" render={CustomErrorMessage} />
-              </div>
+                  <div className="form-group">
+                    <label
+                      htmlFor="message"
+                      className="form-label"
+                      // eslint-disable-next-line react/no-unknown-property
+                      asterisk={isRequired({
+                        name: "message",
+                      }).toString()}
+                    >
+                      Message
+                    </label>
+                    <Field
+                      name="message"
+                      as="textarea"
+                      className="form-control"
+                    />
+                    <ErrorMessage name="message" render={CustomErrorMessage} />
+                  </div>
 
-              <div className="flex justify-end mt-3">
-                <Button submit accent disabled={isLoading} loading={isLoading}>
-                  <div>Submit</div>
-                </Button>
-              </div>
-            </Form>
-          </Formik>
+                  <div className="flex justify-end mt-3">
+                    <Button
+                      submit
+                      accent
+                      disabled={isLoading}
+                      loading={isLoading}
+                    >
+                      <div>Submit</div>
+                    </Button>
+                  </div>
+                </Form>
+              </Formik>
+            </article>
+          </section>
         </PageSection>
       </main>
     </Layout>
