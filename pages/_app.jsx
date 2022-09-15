17e7ad0e9f23b4d5763c3243b4e1446af90d2ed7
madeeha-anjum <maadeeeehaa@gmail.com>
2022-09-15 15:28:59 -0600

    script tag update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,15 +11,13 @@ function MyApp({ Component, pageProps }) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script id="gtag-load" strategy="lazyOnload">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-            });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config',  '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}' );
         `}
       </Script>
 
