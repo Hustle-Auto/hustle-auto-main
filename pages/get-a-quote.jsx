1390318dbc4347427361cf6897709a99bff94471
Pranav Bodawala <b.pranav.k@gmail.com>
2022-10-07 12:23:45 -0600

    dont reset addons and detailing location when changing packages

--- pages/get-a-quote.jsx
+++ pages/get-a-quote.jsx
@@ -72,8 +72,6 @@ const GetAQuote = () => {
     const newUserInput = {
       ...userInput,
       service: selectedService,
-      addOns: [],
-      detailingLocation: null,
     };
 
     setUserInput(newUserInput);
