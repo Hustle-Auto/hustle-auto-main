f5c939ce4b00dae66a30d094c73fa87da2f472b8
Pranav Bodawala <b.pranav.k@gmail.com>
2023-04-12 14:29:46 -0600

    basically done gallery page

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,65 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { Storage } from "@google-cloud/storage";
-import axios from "axios";
 import dayjs from "dayjs";
 import Head from "next/head";
 import Image from "next/image";
 import Masonry from "react-masonry-css";
 
 import Icon from "../components/icon";
-import Card from "../components/ui/Card";
 import PageSection from "../components/ui/PageSection";
 
-const BUCKET_NAME = "pranav-hustleauto-bucket";
+const hasUndefinedKeys = (obj) => {
+  return Object.values(obj).some((value) => value === undefined);
+};
 
-// This function gets called at build time
 export async function getServerSideProps() {
-  // Call an external API endpoint to get posts
+  const BUCKET_NAME = "hustleauto-bkt";
   const storage = new Storage();
 
-  async function getAllImagesData() {
-    const [files] = await storage.bucket(BUCKET_NAME).getFiles();
+  let files = [];
 
-    const imagesData = files
-      // All the important information is in the metadata property
-      .map((file) => file.metadata)
-      // Get all the information we are interested in
-      .map((fileMetadata) => ({
-        id: fileMetadata.id,
-        url: fileMetadata.mediaLink,
-        updated: fileMetadata.updated,
-        width: fileMetadata.metadata.width,
-        height: fileMetadata.metadata.height,
-      }))
-      // Sort by date
-      .sort((a, b) => (dayjs(a.updated).isAfter(dayjs(b.updated)) ? 1 : -1));
-
-    return imagesData;
+  try {
+    [files] = await storage.bucket(BUCKET_NAME).getFiles();
+  } catch (error) {
+    console.log(`Error getting files from bucket '${BUCKET_NAME}'`);
+    console.log(JSON.stringify(error));
   }
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+  const imagesData = files
+    // All the important information is in the metadata property
+    .map((file) => {
+      return file.metadata;
+    })
+    // Get all the information we are interested in
+    .map((fileMetadata) => ({
+      id: fileMetadata.id,
+      url: fileMetadata.mediaLink,
+      updated: fileMetadata.updated,
+      width: fileMetadata.metadata?.width,
+      height: fileMetadata.metadata?.height,
+      tag: fileMetadata.metadata?.tag,
+      link: fileMetadata.metadata?.link ?? "",
+    }))
+    // Remove images that don't have all the information we need
+    .filter((imageData) => !hasUndefinedKeys(imageData))
+    // Sort by date
+    .sort((a, b) => (dayjs(a.updated).isAfter(dayjs(b.updated)) ? -1 : 1));
+
   return {
     props: {
-      imagesData: await getAllImagesData(),
+      imagesData: imagesData,
     },
   };
 }
-
-// const getAllImagesData = async () => {
-//   const res = await axios.get(
-//     `https://storage.googleapis.com/storage/v1/b/${BUCKET_NAME}/o`
-//   );
-
-//   return res.data.items.map((imageData) => ({
-//     id: imageData.id,
-//     url: imageData.mediaLink,
-//     updated: imageData.updated,
-//     width: imageData.metadata.width,
-//     height: imageData.metadata.height,
-//   }));
-// };
 
 const Gallery = ({ imagesData }) => {
   return (
@@ -83,30 +75,49 @@ const Gallery = ({ imagesData }) => {
             >
               {imagesData.map((image) => {
                 return (
-                  <a
+                  <section
                     key={image.url}
-                    className="relative block overflow-hidden transition duration-500 hover:bg-accent group"
-                    href={image.url}
+                    className="block border rounded shadow-sm"
+                    href={image.instagram_link}
+                    target="_blank"
+                    rel="noreferrer"
                   >
-                    <div className="w-full overflow-hidden transition duration-500 rounded group-hover:opacity-70 group-hover:scale-105">
-                      <Image
-                        src={image.url}
-                        alt="Image"
-                        width={image.width}
-                        height={image.height}
-                        layout="responsive"
-                      />
-                    </div>
-
-                    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full transition duration-500 opacity-0 group-hover:opacity-100">
-                      <div className="px-4 py-4 rounded bg-black/70">
-                        <div className="flex space-x-2">
-                          <Icon.Instagram className="w-6 h-6" />
-                          <span className="text-white ">Instagram</span>
-                        </div>
+                    <a className="relative block overflow-hidden transition duration-500 hover:bg-white group">
+                      <div className="w-full overflow-hidden transition duration-500 group-hover:opacity-100 group-hover:scale-105">
+                        <Image
+                          src={image.url}
+                          alt="Image"
+                          width={image.width}
+                          height={image.height}
+                          layout="responsive"
+                        />
                       </div>
-                    </div>
-                  </a>
+
+                      {/* <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full transition duration-500 opacity-0 group-hover:opacity-100">
+                        <div className="px-4 py-4 rounded bg-black/70">
+                          <div className="flex space-x-2">
+                            <Icon.Instagram className="w-6 h-6" />
+                            <span className="text-white ">Instagram</span>
+                          </div>
+                        </div>
+                      </div> */}
+                    </a>
+                    <article className="flex justify-between px-3 my-4">
+                      <div className="px-3 py-1 text-sm text-white bg-black rounded-full">
+                        {image.tag}
+                      </div>
+
+                      {image.link && (
+                        <a className="flex items-center space-x-2 group">
+                          <Icon.Instagram className="w-6 h-6" />
+                          <div>
+                            <span className="text-sm underline">Instagram</span>
+                            <Icon.ExternalLink className="inline-block w-4 h-4 ml-2" />
+                          </div>
+                        </a>
+                      )}
+                    </article>
+                  </section>
                 );
               })}
             </Masonry>
