192d236b11b2163c76e5a9a59a455e90fa67e935
Pranav Bodawala <b.pranav.k@gmail.com>
2024-04-28 21:45:16 -0600

    refactor: Remove complete packages

--- data/services.js
+++ data/services.js
@@ -72,7 +72,6 @@ export const carTypes = [
 
 export const serviceTypes = [
   { id: "interior", label: "Interior Detailing" },
-  { id: "complete", label: "Complete Packages" },
   { id: "paint-correction", label: "Paint Correction" },
   { id: "exterior", label: "Exterior Detailing" },
 ];
@@ -196,93 +195,6 @@ export const services = [
       "mid-size-suv": 282.0,
       "pickup-truck": 315.0,
       "full-size-suv": 315.0,
-    },
-  },
-  {
-    id: "regular-complete-package",
-    label: "Regular Complete Detailing",
-    serviceType: "complete",
-    description: `
-    <p>Interior</p>
-    <ul>
-      <li>A thorough vacuum of the interior carpets, mats, seats and truck space</li>
-      <li>Clean and sanitize all interior panels</li>
-      <li>Windows cleaned from both sides</li>
-      <li>Steam clean of the vents</li>
-      <li>Wash rubber mats</li>
-    </ul>
-    <p>Exterior</p>
-    <ul>
-      <li>Two bucket hand wash</li>
-      <li>Cleaning of tires and rims </li>
-      <li>Conditioning of tire with tire dressing</li>
-      <li>Cleaning of all exterior windows</li>
-    </ul>
-    `,
-    prices: {
-      sedan: 198.9,
-      "mid-size-suv": 264.9,
-      "pickup-truck": 281.4,
-      "full-size-suv": 314.4,
-    },
-  },
-  {
-    id: "enhanced-complete-package",
-    label: "Enhanced Complete Detailing",
-    serviceType: "complete",
-    description: `
-    <p>Interior</p>
-    <ul>
-      <li>Includes everything from the Regular Interior Detail</li>
-      <li>Shampoo of mats, carpets of the vehicle and if vehicle is equipped with cloths seats, they will be shampooed as well</li>
-      <li>Clean and condition leather seats</li>
-      <li>Clean and condition vinyl and plastics</li>
-      <li>Remove any spots</li>
-      <li>Cleaning of door jams</li>
-    </ul>
-    <p>Exterior</p>
-    <ul>
-      <li>Includes everything in the Regular Exterior </li>
-      <li>Decontamination wash </li>
-      <li>Clay bar the vehicle, removing any containments in the paint</li>
-      <li>A wax/sealant applied to the vehicle providing a deeper shine and protection to the vehicle</li>
-    </ul>
-    `,
-    prices: {
-      sedan: 364.9,
-      "mid-size-suv": 430.9,
-      "pickup-truck": 463.9,
-      "full-size-suv": 496.9,
-    },
-  },
-  {
-    id: "elite-complete-package",
-    label: "Elite Complete Detailing",
-    serviceType: "complete",
-    description: `
-    <p>Interior</p>
-    <ul>
-      <li>Includes everything from Enhanced Interior detail</li>
-      <li>Cleaning of the headliner</li>
-      <li>Steam clean of seat belts</li>
-      <li>Protection coating of all Plastic, Vinyl and Rubber surfaces from stains and dirt</li>
-      <li>Protection coating on all leather surfaces</li>
-      <li>Protection coating on all fabric surfaces</li>
-    </ul>
-    <p>Exterior</p>
-    <ul>
-      <li>Includes everything in the Enhanced Detail </li>
-      <li>Iron remover</li>
-      <li>A full exterior stage 1 polish</li>
-      <li>Wax to help protect paint, and bring a  clear and enriched polish </li>
-      <li>Protection on all plastic and rubber parts </li>
-    </ul>
-    `,
-    prices: {
-      sedan: 547.4,
-      "mid-size-suv": 596.9,
-      "pickup-truck": 629.9,
-      "full-size-suv": 662.9,
     },
   },
   {
